fe9b83bdf95a75081b904c173314369daa87e610
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-14 17:17:40 +0100

    add markup and styles

--- src/components/Claims/index.tsx
+++ src/components/Claims/index.tsx
@@ -1,1 +1,21 @@
 import "./index.scss";
+
+export const Claims = () => {
+  return (
+    <article className="claims">
+      <div className="claims__title">
+        <h3>候選主張</h3>
+        <h4>Candidate Claims</h4>
+      </div>
+      <div className="claims__intro">
+        <p>
+          我黑新星堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的
+          GDP 經濟帶來巨大效益。
+        </p>
+        <p>
+          因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。讓我們一同護航台灣的幸福經濟，從照顧每一隻貓咪開始。
+        </p>
+      </div>
+    </article>
+  );
+};
