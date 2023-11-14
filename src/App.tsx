c2c67f6e2c15ce3a95654b91e072187362ad07ee
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-14 16:26:10 +0100

    add main component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
 import HeaderContainer from "./containers/HeaderContainer";
 import { BlurMessage } from "./containers/BlurContext";
+import { Main } from "./components/Main";
 function App() {
   return (
     <>
       <BrowserRouter>
         <BlurMessage>
           <HeaderContainer />
+          <Main />
         </BlurMessage>
       </BrowserRouter>
     </>
