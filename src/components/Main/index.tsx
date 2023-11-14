9c0edff2046d60fa76ea9d20eaf1a3805416ad39
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-14 17:17:00 +0100

    import claim component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,13 +2,14 @@ import "./index.scss";
 import { useContext } from "react";
 import { BlurContext, BlurContextType } from "../../containers/BlurContext";
 import { Hero } from "../Hero";
-
+import { Claims } from "../Claims";
 export const Main = () => {
   const { navOpen } = useContext(BlurContext) as BlurContextType;
 
   return (
     <main className={`main ${navOpen ? "blur" : ""}`}>
       <Hero />
+      <Claims />
     </main>
   );
 };
