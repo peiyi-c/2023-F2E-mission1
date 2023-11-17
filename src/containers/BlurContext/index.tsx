cb3ef65bd0f820477b2d1337ef5f0098cbd35b58
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-17 20:52:16 +0100

    rename state

--- src/containers/BlurContext/index.tsx
+++ src/containers/BlurContext/index.tsx
@@ -5,17 +5,17 @@ type BlurMessageProps = {
 };
 
 export type BlurContextType = {
-  navOpen?: boolean;
-  setNavOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  blur?: boolean;
+  setBlur?: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export const BlurContext = createContext({});
 
 export const BlurMessage = ({ children }: BlurMessageProps) => {
-  const [navOpen, setNavOpen] = useState(false);
+  const [blur, setBlur] = useState(false);
 
   return (
-    <BlurContext.Provider value={{ navOpen, setNavOpen }}>
+    <BlurContext.Provider value={{ blur, setBlur }}>
       {children}
     </BlurContext.Provider>
   );
